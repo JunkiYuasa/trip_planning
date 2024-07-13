95da08f5d6e11fa123d1455daff5f7188e3d2a3d
EC2 Default User <ec2-user@ip-172-31-21-176.ap-northeast-1.compute.internal>
2024-07-13 14:15:40 +0000

    top画面の編集

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,10 @@
 class Public::HomesController < ApplicationController
   def top
+    if user_signed_in?
+      redirect_to user_path(current_user)
+    elsif admin_signed_in?
+      redirect_to admin_path
+    end
   end
 
   def about
